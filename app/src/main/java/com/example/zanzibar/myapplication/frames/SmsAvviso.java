1f737d605f37af673aa2036b32ecd0587853b1bc
vale17accidentidellastoria <pingupucca@gmail.com>
2018-05-29 11:28:26 +0200

    impostato sms avviso

--- app/src/main/java/com/example/zanzibar/myapplication/frames/SmsAvviso.java
+++ app/src/main/java/com/example/zanzibar/myapplication/frames/SmsAvviso.java
@@ -20,6 +20,8 @@ public class SmsAvviso extends Fragment {
 
     private LinearLayout linearLayout = null;
 
+    private LinearLayout layout_contattiSMS = null;
+
     public SmsAvviso() {
         // Required empty public constructor
     }
@@ -36,14 +38,26 @@ public class SmsAvviso extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         linearLayout = (LinearLayout) view.findViewById(R.id.llayoutsms);
+        layout_contattiSMS = (LinearLayout) view.findViewById(R.id.myview2);
         View frame = LayoutInflater.from(getActivity()).inflate(R.layout.frame_sms, linearLayout, false);
+
+
+
         linearLayout.addView(frame);
+        for(int i = 0; i < 2; i++){
+            addLayoutContattiSMS();
+        }
     }
 
     @Override
     public void onResume(){
         super.onResume();
         ((MainActivity) getActivity()).setActionBarTitle("SMS AVVISO");
+    }
+
+    private void addLayoutContattiSMS() {
+        View frame_contatti = LayoutInflater.from(getActivity()).inflate(R.layout.frame_contatti, layout_contattiSMS, false);
+        linearLayout.addView(frame_contatti);
     }
 
 }
