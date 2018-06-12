5dab9282072cb453b0ab685fb154757772bab321
vale17accidentidellastoria <pingupucca@gmail.com>
2018-06-12 17:16:12 +0200

    ultimata gestione delle impostazioni di notifica attuali del frame Impostazioni

--- app/src/main/java/com/example/zanzibar/myapplication/Session.java
+++ app/src/main/java/com/example/zanzibar/myapplication/Session.java
@@ -3,6 +3,8 @@ package com.example.zanzibar.myapplication;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import static android.content.Context.MODE_PRIVATE;
+
 /**
  * Created by Akshay Raj on 7/28/2016.
  * Snow Corporation Inc.
@@ -10,7 +12,9 @@ import android.content.SharedPreferences;
  */
 public class Session {
     SharedPreferences pref;
+    SharedPreferences prefsettings;
     SharedPreferences.Editor editor;
+    SharedPreferences.Editor editorprefsettings;
     Context _context;
 
     // shared pref mode
@@ -18,18 +22,29 @@ public class Session {
 
     // Shared preferences file name
     private static final String PREF_NAME = "snow-intro-slider";
+    private static final String PREF_SETTINGS_NAME = "ImpostazioniNotifiche";
 
     private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
 
     public Session(Context context) {
         this._context = context;
         pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        prefsettings = context.getSharedPreferences(PREF_SETTINGS_NAME, PRIVATE_MODE);
         editor = pref.edit();
+        editorprefsettings = prefsettings.edit();
     }
 
     public void setFirstTimeLaunch(boolean isFirstTime) {
         editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
         editor.commit();
+        if(isFirstTime) {
+            editorprefsettings.putBoolean("imposta_notifiche_app", true);
+            editorprefsettings.putBoolean("imposta_notifiche_sms", true);
+            editorprefsettings.putBoolean("imposta_notifiche_scorta_app", true);
+            editorprefsettings.putBoolean("imposta_notifiche_farmaci", true);
+            editorprefsettings.putString("minuti_smsavviso", "30 minuti");
+            editorprefsettings.apply();
+        }
     }
 
     public boolean isFirstTimeLaunch() {
--- app/src/main/java/com/example/zanzibar/myapplication/settings/FragmentNotifiche.java
+++ app/src/main/java/com/example/zanzibar/myapplication/settings/FragmentNotifiche.java
@@ -2,14 +2,11 @@ package com.example.zanzibar.myapplication.settings;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v14.preference.SwitchPreference;
-import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.SwitchPreferenceCompat;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.example.zanzibar.myapplication.R;
 
@@ -19,30 +16,70 @@ public class FragmentNotifiche extends PreferenceFragmentCompat {
 
     boolean bool = true;
     SharedPreferences.Editor editor = null;
+    SharedPreferences prefs = null;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         // Load the preferences from an XML resource
         setPreferencesFromResource(R.xml.pref_notifiche, rootKey);
         editor = getContext().getSharedPreferences("ImpostazioniNotifiche",MODE_PRIVATE).edit();
+        prefs = getContext().getSharedPreferences("ImpostazioniNotifiche", MODE_PRIVATE);
 
         //-----------------------------
 
         final SwitchPreferenceCompat notifiche_app = (SwitchPreferenceCompat) findPreference("notifiche_abilitate");
-        notifiche_app.setChecked(true);
+        boolean enable_notifications = prefs.getBoolean("imposta_notifiche_app",false);
+        Log.i("1", enable_notifications+"");
+        if(enable_notifications) {
+            notifiche_app.setChecked(true);
+        } else {
+            notifiche_app.setChecked(false);
+        }
+
+        final SwitchPreferenceCompat notifiche_scorta = (SwitchPreferenceCompat) findPreference("notifiche_scorta_abilitate");
+        boolean enable_scorta = prefs.getBoolean("imposta_notifiche_scorta_app",false);
+        Log.i("2", enable_scorta+"");
+        if(enable_scorta) {
+            notifiche_scorta.setChecked(true);
+        } else {
+            notifiche_scorta.setChecked(false);
+        }
+
+        final SwitchPreferenceCompat notifiche_SMSAVVISO = (SwitchPreferenceCompat) findPreference("notifiche_SMSAVVISO_abilitate");
+        boolean enable_sms = prefs.getBoolean("imposta_notifiche_sms",false);
+        Log.i("3", enable_sms+"");
+        if(enable_sms) {
+            notifiche_SMSAVVISO.setChecked(true);
+        } else {
+            notifiche_SMSAVVISO.setChecked(false);
+        }
+
+        final ListPreference tempo_smsavviso = (ListPreference) findPreference("minuti_smsmavviso");
+        String minutiSMS = prefs.getString("minuti_smsavviso","");
+        tempo_smsavviso.setSummary(minutiSMS);
+
+        final SwitchPreferenceCompat notifiche_assunzione = (SwitchPreferenceCompat) findPreference("notifiche_assunzione_farmaci");
+        boolean enable_farmaci = prefs.getBoolean("imposta_notifiche_farmaci",false);
+        Log.i("4", enable_farmaci+"");
+        if(enable_farmaci) {
+            notifiche_assunzione.setChecked(true);
+        } else {
+            notifiche_assunzione.setChecked(false);
+        }
+
+        //-----------------------------
+
         notifiche_app.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick (Preference preference) {
                 if(notifiche_app.isChecked()) {
                     editor.putBoolean("imposta_notifiche_app", true);
                     editor.apply();
-                    SharedPreferences prefs = getContext().getSharedPreferences("ImpostazioniNotifiche", MODE_PRIVATE);
                     boolean b = prefs.getBoolean("imposta_notifiche_app",false);
                     Log.i("notifiche abilitate1_1", b+"");
                 } else if (!(notifiche_app.isChecked())){
                     editor.putBoolean("imposta_notifiche_app", false);
                     editor.apply();
-                    SharedPreferences prefs = getContext().getSharedPreferences("ImpostazioniNotifiche", MODE_PRIVATE);
                     boolean b = prefs.getBoolean("imposta_notifiche_app",false);
                     Log.i("notifiche abilitate1_2", b+"");
                 }
@@ -50,23 +87,17 @@ public class FragmentNotifiche extends PreferenceFragmentCompat {
             }
         });
 
-        //-----------------------------
-
-        final SwitchPreferenceCompat notifiche_scorta = (SwitchPreferenceCompat) findPreference("notifiche_scorta_abilitate");
-        notifiche_scorta.setChecked(true);
         notifiche_scorta.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick (Preference preference) {
                 if(notifiche_scorta.isChecked()) {
                     editor.putBoolean("imposta_notifiche_scorta_app", true);
                     editor.apply();
-                    SharedPreferences prefs = getContext().getSharedPreferences("ImpostazioniNotifiche", MODE_PRIVATE);
                     boolean b = prefs.getBoolean("imposta_notifiche_scorta_app",false);
                     Log.i("notifiche abilitate2_1", b+"");
                 } else if (!(notifiche_scorta.isChecked())){
                     editor.putBoolean("imposta_notifiche_scorta_app", false);
                     editor.apply();
-                    SharedPreferences prefs = getContext().getSharedPreferences("ImpostazioniNotifiche", MODE_PRIVATE);
                     boolean b = prefs.getBoolean("imposta_notifiche_scorta_app",false);
                     Log.i("notifiche abilitate2_2", b+"");
                 }
@@ -74,23 +105,17 @@ public class FragmentNotifiche extends PreferenceFragmentCompat {
             }
         });
 
-        //-----------------------------
-
-        final SwitchPreferenceCompat notifiche_SMSAVVISO = (SwitchPreferenceCompat) findPreference("notifiche_SMSAVVISO_abilitate");
-        notifiche_SMSAVVISO.setChecked(true);
         notifiche_SMSAVVISO.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick (Preference preference) {
                 if(notifiche_SMSAVVISO.isChecked()) {
                     editor.putBoolean("imposta_notifiche_sms", true);
                     editor.apply();
-                    SharedPreferences prefs = getContext().getSharedPreferences("ImpostazioniNotifiche", MODE_PRIVATE);
                     boolean b = prefs.getBoolean("imposta_notifiche_sms",false);
                     Log.i("notifiche abilitate3_1", b+"");
                 } else if (!(notifiche_SMSAVVISO.isChecked())){
                     editor.putBoolean("imposta_notifiche_sms", false);
                     editor.apply();
-                    SharedPreferences prefs = getContext().getSharedPreferences("ImpostazioniNotifiche", MODE_PRIVATE);
                     boolean b = prefs.getBoolean("imposta_notifiche_sms",false);
                     Log.i("notifiche abilitate3_2", b+"");
                 }
@@ -98,44 +123,35 @@ public class FragmentNotifiche extends PreferenceFragmentCompat {
             }
         });
 
-        final ListPreference tempo_smsavviso = (ListPreference) findPreference("minuti_smsmavviso");
         tempo_smsavviso.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                /*
                 CharSequence currText = tempo_smsavviso.getEntry();
                 String currValue = tempo_smsavviso.getValue();
                 Log.i("1", currText+"");
                 Log.i("2", currValue);
+                //bug di ListPreference: non aggiorna summary automaticamente!!!!
                 tempo_smsavviso.setSummary(currText);
-                /*
-                editor.putBoolean("minuti_smsavviso", true);
+                editor.putString("minuti_smsavviso", currValue);
                 editor.apply();
                 SharedPreferences prefs = getContext().getSharedPreferences("ImpostazioniNotifiche", MODE_PRIVATE);
-                boolean b = prefs.getBoolean("imposta_notifiche_farmaci",false);
-                Log.i("notifiche abilitate4_1", b+"");
-                */
+                String b = prefs.getString("minuti_smsavviso","");
+                Log.i("stringa minuti", b);
                 return true;
             }
         });
 
-        //-----------------------------
-
-        final SwitchPreferenceCompat notifiche_assunzione = (SwitchPreferenceCompat) findPreference("notifiche_assunzione_farmaci");
-        notifiche_assunzione.setChecked(true);
         notifiche_assunzione.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick (Preference preference) {
                 if(notifiche_assunzione.isChecked()) {
                     editor.putBoolean("imposta_notifiche_farmaci", true);
                     editor.apply();
-                    SharedPreferences prefs = getContext().getSharedPreferences("ImpostazioniNotifiche", MODE_PRIVATE);
                     boolean b = prefs.getBoolean("imposta_notifiche_farmaci",false);
                     Log.i("notifiche abilitate4_1", b+"");
                 } else if (!(notifiche_assunzione.isChecked())){
                     editor.putBoolean("imposta_notifiche_farmaci", false);
                     editor.apply();
-                    SharedPreferences prefs = getContext().getSharedPreferences("ImpostazioniNotifiche", MODE_PRIVATE);
                     boolean b = prefs.getBoolean("imposta_notifiche_farmaci",false);
                     Log.i("notifiche abilitate4_2", b+"");
                 }
